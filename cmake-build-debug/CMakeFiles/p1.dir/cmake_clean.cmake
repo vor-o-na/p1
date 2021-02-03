eda61e8c9054e1337bf58895e31bb2a9b9c92f20
vorona <emili20032003@gmail.com>
2021-02-03 22:14:06 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/p1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1.dir/main.cpp.o"
+  "p1"
+  "p1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
